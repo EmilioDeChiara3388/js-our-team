6ea7f4c5093e23112f1cf1bbaa870c8e76f1ffa7
EmilioDeChiara3388 <emilio.dechiara3388@gmail.com>
2024-10-14 15:38:17 +0200

    add: result on page

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,9 @@
-console.log("Funzioni?");
+//console.log("Funzioni?");
+
+/* 
+Dato un array di oggetti rappresentante un team di un’azienda, creare una pagina dedicata in cui mostrare una card per ciascun componente.
+(trovate l’array del team all’interno della cartella in allegato)
+*/
 
 const teamMembers = [
   {
@@ -38,3 +43,23 @@ const teamMembers = [
     img: "img/female3.png"
   }
 ];
+
+let rowEl = document.querySelector(".row");
+
+for (let i = 0; i < teamMembers.length; i++) {
+    let teamMember = teamMembers[i];
+    console.log(teamMember);
+    let {name, role, email, img} = teamMember;
+    let markup = `
+    <div class="col-4">
+      <div class="card bg-dark text-light">
+        <img src="${img}" alt="" class="member-pic p-0">
+        <h3>${name}</h3>
+        <p>${role}</p>
+        <p class="text-primary">${email}</p>
+        </div>
+    </div>
+    `
+    rowEl.innerHTML += markup;
+
+}
